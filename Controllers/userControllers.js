4f27436f90125bedf57c740fab5949d79f76237e
Eng_Ahmed <167457758+Eng-Ahmed25@users.noreply.github.com>
2025-05-06 22:24:27 +0300

    Update userControllers.js

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -216,7 +216,7 @@ exports.getAllusers = async (req , res) =>
           });
     
           // Send the image to the FastAPI endpoint for prediction
-          const fastApiUrl = 'https://ai-colone-production.up.railway.app/predict/';
+          const fastApiUrl = 'https://ai-colon-production.up.railway.app/predict/';
           const predictionResponse = await axios.post(fastApiUrl, formData, {
             headers: {
               ...formData.getHeaders(),
